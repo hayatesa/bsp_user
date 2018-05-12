9437a74152dc9c3c6a22a84632350a9a3c3d5b8f
溶酶菌 <hayatesa@live.cn>
2018-05-12 23:13:07 +0800

    移动同意结束到LendController

--- src/main/java/com/bsp/controller/LendOrderController.java
+++ src/main/java/com/bsp/controller/LendOrderController.java
@@ -4,25 +4,19 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bsp.entity.LendingRecord;
 import com.bsp.utils.Result;
 
-/**
- * 借书
- * @author hayate
- *
- */
 @RestController
 @Scope(value="prototype")
-@RequestMapping("borrow")
-public class BorrowController extends BaseController {
+@RequestMapping("lend")
+public class LendOrderController extends BaseController {
 	
 	/**
-	 * 申请借书
-	 * @param record 用于暂时封装参数
+	 * 同意借书
+	 * @param lrId 借书记录id
 	 */
-	@RequestMapping("apply")
-	public Result apply(LendingRecord record) {
+	@RequestMapping("agree")
+	public Result agree(Integer lrId) {
 		return Result.success();
 	}
 	
