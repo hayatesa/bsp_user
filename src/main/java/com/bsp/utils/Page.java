a71e77f57a6106d9b836fffa6b79467a3f498f90
溶酶菌 <hayatesa@live.cn>
2018-05-14 15:09:17 +0800

    修改变量名

--- src/main/java/com/bsp/utils/Page.java
+++ src/main/java/com/bsp/utils/Page.java
@@ -9,70 +9,69 @@ import java.util.List;
 public class Page implements Serializable {
 	private static final long serialVersionUID = 1L;
 	//总记录数
-	private int totalCount;
-	//每页记录数
-	private int pageSize;
-	//总页数
-	private int totalPage;
-	//当前页数
-	private int currPage;
-	//列表数据
-	private List<?> list;
+		private int total;
+		//每页记录数
+		private int limit;
+		//总页数
+		private int totalPage;
+		//当前页数
+		private int currPage;
+		//列表数据
+		private List<?> rows;
 
-	/**
-	 * 分页
-	 * @param list        列表数据
-	 * @param totalCount  总记录数
-	 * @param pageSize    每页记录数
-	 * @param currPage    当前页数
-	 */
-	public Page(List<?> list, int totalCount, int pageSize, int currPage) {
-		this.list = list;
-		this.totalCount = totalCount;
-		this.pageSize = pageSize;
-		this.currPage = currPage;
-		this.totalPage = (int)Math.ceil((double)totalCount/pageSize);
-	}
+		/**
+		 * 分页
+		 * @param list        列表数据
+		 * @param totalCount  总记录数
+		 * @param pageSize    每页记录数
+		 * @param currPage    当前页数
+		 */
+		public Page(List<?> list, int totalCount, int pageSize, int currPage) {
+			this.rows = list;
+			this.total = totalCount;
+			this.limit = pageSize;
+			this.currPage = currPage;
+			this.totalPage = (int)Math.ceil((double)totalCount/pageSize);
+		}
 
-	public int getTotalCount() {
-		return totalCount;
-	}
+		public int getTotalCount() {
+			return total;
+		}
 
-	public void setTotalCount(int totalCount) {
-		this.totalCount = totalCount;
-	}
+		public void setTotalCount(int totalCount) {
+			this.total = totalCount;
+		}
 
-	public int getPageSize() {
-		return pageSize;
-	}
+		public int getPageSize() {
+			return limit;
+		}
 
-	public void setPageSize(int pageSize) {
-		this.pageSize = pageSize;
-	}
+		public void setPageSize(int pageSize) {
+			this.limit = pageSize;
+		}
 
-	public int getTotalPage() {
-		return totalPage;
-	}
+		public int getTotalPage() {
+			return totalPage;
+		}
 
-	public void setTotalPage(int totalPage) {
-		this.totalPage = totalPage;
-	}
+		public void setTotalPage(int totalPage) {
+			this.totalPage = totalPage;
+		}
 
-	public int getCurrPage() {
-		return currPage;
-	}
+		public int getCurrPage() {
+			return currPage;
+		}
 
-	public void setCurrPage(int currPage) {
-		this.currPage = currPage;
-	}
+		public void setCurrPage(int currPage) {
+			this.currPage = currPage;
+		}
 
-	public List<?> getList() {
-		return list;
-	}
+		public List<?> getList() {
+			return rows;
+		}
 
-	public void setList(List<?> list) {
-		this.list = list;
-	}
-
+		public void setList(List<?> list) {
+			this.rows = list;
+		}
 
 }
