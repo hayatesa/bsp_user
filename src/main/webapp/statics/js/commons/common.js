ce91f6bc9893cf1feb013b098619a006dd2db1c0
溶酶菌 <hayatesa@live.cn>
2018-05-16 00:40:02 +0800

    消息页面，+动态页码居中分页条

--- src/main/webapp/statics/js/commons/common.js
+++ src/main/webapp/statics/js/commons/common.js
@@ -85,3 +85,35 @@ function isBlank(value) {
 function millisecondsToDateTime(ms){
 	return new Date(ms).toLocaleString();
 }
+/**
+ * 首页 上页 [1] 2 3 4 5 7 8 9 9 10 下页 末页 当前第 1/18 页 共100条记录 每页x条
+ *
+ * @param totalIndexCount
+ *            总索引数
+ * @param currentPage
+ *            当前页
+ * @param totalPage
+ *            总页数
+ * @return
+ */
+var getPageIndex = function (totalIndexCount, currentPage, totalPage) {
+    var startPageIndex = currentPage - (totalIndexCount % 2 == 0 ? Math.floor(totalIndexCount/2) - 1 : Math.floor(totalIndexCount/2));
+    var endPageIndex = currentPage + Math.floor(totalIndexCount/2);
+    if (startPageIndex < 1) {
+        startPageIndex = 1;
+        if (totalPage >= totalIndexCount) {
+            endPageIndex = totalIndexCount;
+        } else {
+            endPageIndex = totalPage;
+        }
+    }
+    if (endPageIndex > totalPage) {
+        endPageIndex = totalPage;
+        if (endPageIndex - totalIndexCount > 0) {
+            startPageIndex = endPageIndex - totalIndexCount + 1;
+        } else {
+            startPageIndex = 1;
+        }
+    }
+    return {'startPageIndex':parseInt(startPageIndex),'endPageIndex':parseInt(endPageIndex)};
+}
---
+++ src/main/webapp/statics/js/user/message.js
@@ -0,0 +1,61 @@
+var loadUnreadMsg = function(app) {
+    $.ajax({
+        url: '/msg/unread_list',
+        data: app.pageParams,
+        success: function (data) {
+            if (data.code==0) {
+                app.page=data.page;
+                // 设置分页工具条
+                var pageIndex = getPageIndex(app.pageBar.pageBarSize, data.page.currPage, data.page.totalPage);
+                app.pageBar.startPageIndex=pageIndex.startPageIndex;
+                app.pageBar.endPageIndex=pageIndex.endPageIndex;
+
+            } else if (data.code==401) { //未登录
+                window.location.href='/login'
+            } else {
+                alert(data.msg);
+            }
+        }
+    });
+    $.ajax({//加载消息数
+        url: '/msg/amount',
+        type:"get",
+        success: function (data) {
+            var newMsgNum = data.msgNum;
+            app.newMsgNum=newMsgNum<100?newMsgNum:'99+';
+        }
+    })
+}
+
+var init = function () {
+    loadUnreadMsg(this);
+}
+
+var msg_app = new Vue({
+    el: "#msg-app",
+    data: {
+        showPage: 0,//0-显示未读消息,1-显示已读消息
+        newMsgNum: 0,//未读消息数
+        page: {}, // 分页数据
+        pageBar: { // 分页工具条参数
+            pageBarSize: 3,// 分页工具条大小
+            startPageIndex: 1, // 分页工具条开始页码
+            endPageIndex: 1, // 分页工具条结束页码
+        },
+        pageParams: {// 分页查询参数
+            limit: 12, // 页大小
+            pageNumber: 1 // 当前页
+        },
+    },
+    methods: {
+        loadUnreadMsg: loadUnreadMsg,
+        goPage: function (currPage) {// 页面跳转
+            this.pageParams.pageNumber = currPage;
+            this.loadUnreadMsg(this);
+        },
+        millisecondsToDateTime: function (ms){
+            return new Date(ms).toLocaleString();
+        }
+    },
+    created: init
+})
