c3a7e5cc25bf486fa9643830a3523e2ab7ad961d
溶酶菌 <hayatesa@live.cn>
2018-05-19 23:44:11 +0800

    增加MultipartResolver配置

--- src/main/java/com/bsp/config/WebConfig.java
+++ src/main/java/com/bsp/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.bsp.config;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +13,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -51,6 +54,24 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
+	}
+	
+	/**
+	 * 使用CommonsMultipartResolver处理multipart请求
+	 * 配置CommonsMultipartResolver的具体细节
+	 * 临时文件保存位置
+	 * 限制文件大小不超过2M
+	 * 无法设置请求整体的最大容量
+	 * 如果文件大小达到最大容量将会写入到临时文件路径中，默认值为0，也就是说所有上传文件都会写入磁盘中
+	 * @throws IOException
+	 */
+	@Bean
+	public MultipartResolver multipartResolver() throws IOException {
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setMaxUploadSize(2097152);
+		multipartResolver.setMaxInMemorySize(0);
+		multipartResolver.setDefaultEncoding("UTF-8");
+		return multipartResolver;
 	}
 	
 	/**
