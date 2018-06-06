b85c23943cae465b2b49e679fe88359e62a5b7cf
溶酶菌 <hayatesa@live.cn>
2018-06-06 17:42:04 +0800

    添加字段

--- src/main/java/com/bsp/entity/DonatedBook.java
+++ src/main/java/com/bsp/entity/DonatedBook.java
@@ -1,12 +1,14 @@
 package com.bsp.entity;
 
-import org.apache.ibatis.type.Alias;
+import java.util.Date;
 
-@Alias("DonatedBook")
 public class DonatedBook extends BaseEntity {
+    /**
+	 * 
+	 */
 	private static final long serialVersionUID = 1L;
 
-	// 捐赠的图书标识，数字自增值
+	// 捐赠的图书标识，自增
     private Integer dobId;
 
     // 捐赠的图书名称
@@ -18,21 +20,34 @@ public class DonatedBook extends BaseEntity {
     // 捐赠的图书数量
     private Integer number;
 
+
     // 捐赠图书所属的二级分类
     private SecondaryClassification secondaryClassification;
 
     // 捐赠人
     private User user;
 
+    // 捐赠来源
+    private String source;
+
+    // 捐赠人
+    private String donor;
+
+    // 捐赠时间
+    private Date time;
+
+    // 联系电话
+    private String phone;
+
     /**
-     * 捐赠的图书标识，数字自增值
+     * 捐赠的图书标识，自增
      */
     public Integer getDobId() {
         return dobId;
     }
 
     /**
-     * @param dobId 捐赠的图书标识，数字自增值
+     * @param dobId 捐赠的图书标识，自增
      */
     public void setDobId(Integer dobId) {
         this.dobId = dobId;
@@ -95,16 +110,72 @@ public class DonatedBook extends BaseEntity {
     }
 
     /**
-     * 捐赠人
+     * 捐赠人账户
      */
     public User getUser() {
         return user;
     }
 
     /**
-     * @param uuid 捐赠人
+     * @param user 捐赠人账户
      */
     public void setUser(User user) {
         this.user = user;
+    }
+
+    /**
+     * 捐赠来源
+     */
+    public String getSource() {
+        return source;
+    }
+
+    /**
+     * @param source 捐赠来源
+     */
+    public void setSource(String source) {
+        this.source = source == null ? null : source.trim();
+    }
+
+    /**
+     * 捐赠人
+     */
+    public String getDonor() {
+        return donor;
+    }
+
+    /**
+     * @param donor 捐赠人
+     */
+    public void setDonor(String donor) {
+        this.donor = donor == null ? null : donor.trim();
+    }
+
+    /**
+     * 捐赠时间
+     */
+    public Date getTime() {
+        return time;
+    }
+
+    /**
+     * @param time 捐赠时间
+     */
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    /**
+     * 联系电话
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone 联系电话
+     */
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
     }
 }
