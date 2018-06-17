854c2e56cd307ef4411fc3171e46f0b470ba83e0
溶酶菌 <hayatesa@live.cn>
2018-06-17 13:31:20 +0800

    添加用户uuid字段，

--- src/main/java/com/bsp/dto/OrderQueryObject.java
+++ src/main/java/com/bsp/dto/OrderQueryObject.java
@@ -8,6 +8,15 @@ package com.bsp.dto;
 public class OrderQueryObject extends QueryObject {
 	
 	private Integer status;// 订单状态，0-进行中 1-异常 2-已结束 3-全部
+	private String uuid;// 用户uuid
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
 
 	public Integer getStatus() {
 		return status;
