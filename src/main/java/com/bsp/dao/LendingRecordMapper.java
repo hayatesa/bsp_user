b1f714cd1ee759b3cf649d97f5132c079e73c093
溶酶菌 <hayatesa@live.cn>
2018-06-17 13:35:57 +0800

    添加用户订单记录查询

--- src/main/java/com/bsp/dao/LendingRecordMapper.java
+++ src/main/java/com/bsp/dao/LendingRecordMapper.java
@@ -1,6 +1,20 @@
 package com.bsp.dao;
 
+import java.util.List;
+
+import com.bsp.dto.OrderQueryObject;
 import com.bsp.entity.LendingRecord;
 
 public interface LendingRecordMapper extends GenericMapper<LendingRecord, Integer> {
+	/**
+	 * 计算分页查询中的记录总数
+	 * @param queryObject 查询对象
+	 */
+	int getTotalCount(OrderQueryObject queryObject);
+	
+	/**
+	 * 分页高级查询
+	 * @param queryObject 查询对象
+	 */
+	List<LendingRecord> selectByQueryObject(OrderQueryObject queryObject);
 }
